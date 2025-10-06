24129ceee3374795bec575c8854e50331aa60713
Joao <232938876+joaokhalaf@users.noreply.github.com>
2025-10-05 22:57:19 -0300

    filtering errors with grep

--- scripts.sh
+++ scripts.sh
@@ -5,5 +5,5 @@ LOG_DIR="./myapp"
 echo "$LOG_DIR"
 
 find $LOG_DIR -name "*.log" -print0 | while IFS= read -r -d '' arquivo; do
-	echo "$arquivo"
+	grep "ERROR" $arquivo > "${arquivo}.filtered"
 done 
